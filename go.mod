cc6091cf5a44f072fcaa237ff4212b6ba1a813d5
unknown <tonysev69@gmail.com>
2025-05-01 21:33:41 +0300

    Change func name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/tonysev69/utils
+module github.com/tonysev69/utils/v2  v1.1.0
 
 go 1.24.0
--- utils.go
+++ utils.go
@@ -1,6 +1,6 @@
 package utils
 
-func Contains(a []string, b string) bool {
+func inSlice(a []string, b string) bool {
 	for _, line := range a {
 		if line == b {
 			return true
